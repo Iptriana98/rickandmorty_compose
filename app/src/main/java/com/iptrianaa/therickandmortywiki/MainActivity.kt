f969a6735b99a2d7b62f5b9bf10ff40300849313
ibrahinperez <iptrianaa@gmail.com>
2023-11-13 06:05:24 -0600

    setup clear project

--- app/src/main/java/com/iptrianaa/therickandmortywiki/MainActivity.kt
+++ app/src/main/java/com/iptrianaa/therickandmortywiki/MainActivity.kt
@@ -4,46 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.paging.LoadState
-import androidx.paging.compose.collectAsLazyPagingItems
-import coil.compose.AsyncImage
 import com.iptrianaa.therickandmortywiki.ui.theme.TheRickAndMortyWikiTheme
 import com.iptrianaa.therickandmortywiki.ui.viewmodel.MainViewModel
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -64,99 +36,98 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(viewModel: MainViewModel) {
-    val paging = viewModel.pager.collectAsLazyPagingItems()
     LazyVerticalGrid(
         contentPadding = PaddingValues(4.dp),
         columns = GridCells.Fixed(2)
     ) {
-        items(items = paging.itemSnapshotList, key = { it!!.url }) { items ->
-            val color = if (items?.status == "Alive") Color.Green else Color.Red
-            Card(modifier = Modifier.padding(4.dp)) {
-                Column {
-                    AsyncImage(
-                        model = items?.image,
-                        modifier = Modifier.fillMaxWidth(),
-                        contentScale = ContentScale.Crop,
-                        contentDescription = "Character's image"
-                    )
-                    Text(
-                        text = items!!.name,
-                        fontSize = 18.sp,
-                        fontWeight = Bold,
-                        maxLines = 2,
-                        modifier = Modifier
-                            .padding(8.dp, 2.dp)
-                            .height(48.dp)
-                    )
-
-                    Row(
-                        modifier = Modifier
-                            .align(Alignment.End)
-                            .padding(4.dp, 0.dp)
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .size(8.dp)
-                                .clip(CircleShape)
-                                .background(color)
-                                .align(CenterVertically)
-                        )
-                        Text(
-                            text = "${items.status} - ${items.species}",
-                            fontSize = 12.sp,
-                            modifier = Modifier.padding(4.dp, 0.dp)
-                        )
-                    }
-                }
-            }
-            when (val state = paging.loadState.refresh) { //FIRST LOAD
-                is LoadState.Error -> {
-                    //TODO Error Item
-                    //state.error to get error message
-                }
-
-                is LoadState.Loading -> { // Loading UI
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center,
-                    ) {
-                        Text(
-                            modifier = Modifier
-                                .padding(8.dp),
-                            text = "Refresh Loading"
-                        )
-
-                        CircularProgressIndicator(color = Color.Black)
-                    }
-                }
-
-                else -> {}
-            }
-
-
-            when (val state = paging.loadState.append) { // Pagination
-                is LoadState.Error -> {
-                    //TODO Pagination Error Item
-                    //state.error to get error message
-                }
-
-                is LoadState.Loading -> { // Pagination Loading UI
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center,
-                    ) {
-                        Text(text = "Pagination Loading")
-
-                        CircularProgressIndicator(color = Color.Black)
-                    }
-                }
-
-                else -> {}
-            }
-        }
+//        items(items = paging.itemSnapshotList, key = { it!!.url }) { items ->
+//            val color = if (items?.status == "Alive") Color.Green else Color.Red
+//            Card(modifier = Modifier.padding(4.dp)) {
+//                Column {
+//                    AsyncImage(
+//                        model = items?.image,
+//                        modifier = Modifier.fillMaxWidth(),
+//                        contentScale = ContentScale.Crop,
+//                        contentDescription = "Character's image"
+//                    )
+//                    Text(
+//                        text = items!!.name,
+//                        fontSize = 18.sp,
+//                        fontWeight = Bold,
+//                        maxLines = 2,
+//                        modifier = Modifier
+//                            .padding(8.dp, 2.dp)
+//                            .height(48.dp)
+//                    )
+//
+//                    Row(
+//                        modifier = Modifier
+//                            .align(Alignment.End)
+//                            .padding(4.dp, 0.dp)
+//                    ) {
+//                        Box(
+//                            modifier = Modifier
+//                                .size(8.dp)
+//                                .clip(CircleShape)
+//                                .background(color)
+//                                .align(CenterVertically)
+//                        )
+//                        Text(
+//                            text = "${items.status} - ${items.species}",
+//                            fontSize = 12.sp,
+//                            modifier = Modifier.padding(4.dp, 0.dp)
+//                        )
+//                    }
+//                }
+//            }
+//            when (val state = paging.loadState.refresh) { //FIRST LOAD
+//                is LoadState.Error -> {
+//                    //TODO Error Item
+//                    //state.error to get error message
+//                }
+//
+//                is LoadState.Loading -> { // Loading UI
+//                    Column(
+//                        modifier = Modifier
+//                            .fillMaxWidth(),
+//                        horizontalAlignment = Alignment.CenterHorizontally,
+//                        verticalArrangement = Arrangement.Center,
+//                    ) {
+//                        Text(
+//                            modifier = Modifier
+//                                .padding(8.dp),
+//                            text = "Refresh Loading"
+//                        )
+//
+//                        CircularProgressIndicator(color = Color.Black)
+//                    }
+//                }
+//
+//                else -> {}
+//            }
+//
+//
+//            when (val state = paging.loadState.append) { // Pagination
+//                is LoadState.Error -> {
+//                    //TODO Pagination Error Item
+//                    //state.error to get error message
+//                }
+//
+//                is LoadState.Loading -> { // Pagination Loading UI
+//                    Column(
+//                        modifier = Modifier
+//                            .fillMaxWidth(),
+//                        horizontalAlignment = Alignment.CenterHorizontally,
+//                        verticalArrangement = Arrangement.Center,
+//                    ) {
+//                        Text(text = "Pagination Loading")
+//
+//                        CircularProgressIndicator(color = Color.Black)
+//                    }
+//                }
+//
+//                else -> {}
+//            }
+//        }
     }
 }
--- app/src/main/java/com/iptrianaa/therickandmortywiki/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/iptrianaa/therickandmortywiki/ui/viewmodel/MainViewModel.kt
@@ -1,28 +1,5 @@
 package com.iptrianaa.therickandmortywiki.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.cachedIn
-import com.iptrianaa.therickandmortywiki.config.datasource.ApiDataSource
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 
-@HiltViewModel
-class MainViewModel @Inject constructor(private val apiDataSource: ApiDataSource): ViewModel() {
-    val pager = Pager(
-        config = PagingConfig(pageSize = 20),
-        pagingSourceFactory = { apiDataSource },
-    ).flow.cachedIn(viewModelScope)
-
-//    fun getCharacters() = viewModelScope.launch {
-//        try {
-//            val response = networkApi.getCharacters()
-//            Log.i("TAG", "getCharacters: ${response.body()}")
-//            if (response.isSuccessful){
-//                _characters.value = response.body()!!.results
-//            }else Log.i("TAG", "getCharacters: ${response.code()}")
-//        }catch (e: Exception){ Log.i("TAG", "getCharacters: ${e.message}") }
-//    }
-}
+class MainViewModel : ViewModel()
