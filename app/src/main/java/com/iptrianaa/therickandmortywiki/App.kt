6cd4c272059e938be25747903d45652f9ff7ee10
ibrahinperez <iptrianaa@gmail.com>
2024-02-27 12:43:40 -0600

    - Created project App Class

---
+++ app/src/main/java/com/iptrianaa/therickandmortywiki/App.kt
@@ -0,0 +1,22 @@
+package com.iptrianaa.therickandmortywiki
+
+import android.app.Application
+import com.iptrianaa.therickandmortywiki.data.di.dataModule
+import com.iptrianaa.therickandmortywiki.di.appModule
+import com.iptrianaa.therickandmortywiki.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(appModule, viewModelModule, dataModule)
+        }
+    }
+}
