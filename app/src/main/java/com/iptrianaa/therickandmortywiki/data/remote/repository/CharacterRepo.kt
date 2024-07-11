93e5abc64da5fa3438f6a8dfa318b8c9a9ceed5e
Ibrahin Perez <iptrianaa@gmail.com>
2024-07-11 13:44:58 -0500

    - modified the character repo to load from pagingSource

--- app/src/main/java/com/iptrianaa/therickandmortywiki/data/remote/repository/CharacterRepo.kt
+++ app/src/main/java/com/iptrianaa/therickandmortywiki/data/remote/repository/CharacterRepo.kt
@@ -1,8 +1,26 @@
 package com.iptrianaa.therickandmortywiki.data.remote.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.iptrianaa.therickandmortywiki.data.config.CharacterService
+import com.iptrianaa.therickandmortywiki.data.remote.CharacterPagingSource
 import com.iptrianaa.therickandmortywiki.data.remote.datasources.character.Character
+import kotlinx.coroutines.flow.Flow
 
 class CharacterRepo(private val service: CharacterService) {
-    suspend fun getCharacters(): List<Character> = service.getCharacters().results
+
+    companion object {
+        const val MAX_ITEMS = 10
+        const val PREFETCH_ITEMS = 3
+    }
+    fun getCharacters(): Flow<PagingData<Character>>{
+        return Pager(
+            config = PagingConfig(
+                pageSize = MAX_ITEMS,
+                enablePlaceholders = true,
+                prefetchDistance = PREFETCH_ITEMS
+            ),
+            pagingSourceFactory = { CharacterPagingSource(service) }).flow
+    }
 }
