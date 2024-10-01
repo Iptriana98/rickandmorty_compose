c3dc70ff591532b8a152e7dd1b6226fa5d94921a
Ibrahin <iptrianaa@gmail.com>
2024-10-01 02:21:18 -0500

    setup bottom bar navigation in home screen

--- composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/ui/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/ui/home/HomeScreen.kt
@@ -1,10 +1,55 @@
 package com.iptriana.rickymortywiki.ui.home
 
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.iptriana.rickymortywiki.ui.core.navigation.bottom_navigation.BottomBarItem
+import com.iptriana.rickymortywiki.ui.core.navigation.bottom_navigation.NavigationBottomWrapper
 
 @Composable
 fun HomeScreen(modifier: Modifier) {
-    Text(text = "Home", modifier = modifier)
+    val items = listOf(BottomBarItem.Episodes(), BottomBarItem.Characters())
+    val navController = rememberNavController()
+
+    Scaffold(bottomBar = { BottomNavigation(modifier, items, navController) }) {
+        NavigationBottomWrapper(navController)
+    }
+}
+
+@Composable
+fun BottomNavigation(
+    modifier: Modifier,
+    items: List<BottomBarItem>,
+    navController: NavHostController
+) {
+    val navBackStackEntry = navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry.value?.destination
+    NavigationBar {
+        items.forEach { item ->
+            NavigationBarItem(
+                selected = currentDestination?.hierarchy?.any { it.route == item.route } == true,
+                label = { Text(text = item.title) },
+                alwaysShowLabel = false,
+                onClick = {
+                    navController.navigate(item.route) {
+                        navController.graph.startDestinationRoute?.let {
+                            popUpTo(it) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
+                icon = { item.icon() },
+            )
+        }
+    }
 }
