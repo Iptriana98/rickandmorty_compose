e678c419e4ec3a2597f93963071a368421805c7f
Ibrahin <iptrianaa@gmail.com>
2024-10-06 12:04:51 -0500

    added EpisodesViewModel

--- composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/di/UIModule.kt
+++ composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/di/UIModule.kt
@@ -1,5 +1,11 @@
 package com.iptriana.rickymortywiki.di
 
+import com.iptriana.rickymortywiki.ui.home.tabs.characters.CharactersViewModel
+import com.iptriana.rickymortywiki.ui.home.tabs.episodes.EpisodesViewModel
+import org.koin.compose.viewmodel.dsl.viewModelOf
 import org.koin.dsl.module
 
-val uiModule = module {}
+val uiModule = module {
+    viewModelOf(::EpisodesViewModel)
+    viewModelOf(::CharactersViewModel)
+}
--- composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/domain/useCases/GetRandomCharacter.kt
+++ composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/domain/useCases/GetRandomCharacter.kt
@@ -1,8 +1,9 @@
 package com.iptriana.rickymortywiki.domain.useCases
 
 import com.iptriana.rickymortywiki.domain.Repository
+import com.iptriana.rickymortywiki.domain.model.CharacterModel
 
 class GetRandomCharacter(private val repository: Repository) {
     private val randomId = (1..826).random()
-    suspend operator fun invoke() = repository.getSingleCharacter(randomId.toString())
+    suspend operator fun invoke(): CharacterModel = repository.getSingleCharacter(randomId.toString())
 }
