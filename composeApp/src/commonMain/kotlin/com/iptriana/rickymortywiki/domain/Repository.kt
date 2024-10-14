6c334a3c2c33f71594f75cc9e10f6099606f7542
Ibrahin <iptrianaa@gmail.com>
2024-10-13 21:51:43 -0500

    modified the Repository

--- composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/domain/Repository.kt
+++ composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/domain/Repository.kt
@@ -1,8 +1,11 @@
 package com.iptriana.rickymortywiki.domain
 
+import androidx.paging.PagingData
 import com.iptriana.rickymortywiki.domain.model.CharacterModel
+import kotlinx.coroutines.flow.Flow
 
 
 interface Repository {
     suspend fun getSingleCharacter(id: String): CharacterModel
+    fun getAllCharacters(): Flow<PagingData<CharacterModel>>
 }
