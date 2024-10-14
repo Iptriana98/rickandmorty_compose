befcec481bcfa3a86134d0548d15cafdc6a2db3b
Ibrahin <iptrianaa@gmail.com>
2024-10-13 21:50:23 -0500

    added the load data to the the viewmodel

--- composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/ui/home/tabs/characters/CharacterState.kt
+++ composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/ui/home/tabs/characters/CharacterState.kt
@@ -1,7 +1,11 @@
 package com.iptriana.rickymortywiki.ui.home.tabs.characters
 
+import app.cash.paging.PagingData
 import com.iptriana.rickymortywiki.domain.model.CharacterModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
 
 data class CharacterState(
     val characterOfTheDay: CharacterModel? = null,
+    val characters: Flow<PagingData<CharacterModel>> = emptyFlow()
 )
