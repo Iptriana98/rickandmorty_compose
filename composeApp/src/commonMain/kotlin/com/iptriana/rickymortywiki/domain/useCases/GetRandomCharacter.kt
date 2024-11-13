4745e1add117c830d73663b6e35d6b1a527ca70c
Ibrahin <iptrianaa@gmail.com>
2024-11-13 05:11:20 -0600

    updating GetRandomCharacter use case

--- composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/domain/useCases/GetRandomCharacter.kt
+++ composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/domain/useCases/GetRandomCharacter.kt
@@ -2,8 +2,20 @@ package com.iptriana.rickymortywiki.domain.useCases
 
 import com.iptriana.rickymortywiki.domain.Repository
 import com.iptriana.rickymortywiki.domain.model.CharacterModel
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 
 class GetRandomCharacter(private val repository: Repository) {
     private val randomId = (1..826).random()
-    suspend operator fun invoke(): CharacterModel = repository.getSingleCharacter(randomId.toString())
+    suspend operator fun invoke(): CharacterModel {
+        repository.getCharacterDB()
+        return repository.getSingleCharacter(randomId.toString())
+    }
+
+    private fun getCurrentDayOfTheYear(): String {
+        val instant = Clock.System.now()
+        val localTime = instant.toLocalDateTime(TimeZone.currentSystemDefault())
+        return "${localTime.dayOfYear}${localTime.year}"
+    }
 }
