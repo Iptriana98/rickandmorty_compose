b35d632585f1de29079e25b38e4d941aa9144147
Ibrahin <iptrianaa@gmail.com>
2024-11-15 04:52:24 -0600

    updating GetRandomCharacter

--- composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/domain/useCases/GetRandomCharacter.kt
+++ composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/domain/useCases/GetRandomCharacter.kt
@@ -2,14 +2,33 @@ package com.iptriana.rickymortywiki.domain.useCases
 
 import com.iptriana.rickymortywiki.domain.Repository
 import com.iptriana.rickymortywiki.domain.model.CharacterModel
+import com.iptriana.rickymortywiki.domain.model.CharacterOfTheDayModel
 import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 
 class GetRandomCharacter(private val repository: Repository) {
-    private val randomId = (1..826).random()
     suspend operator fun invoke(): CharacterModel {
-        repository.getCharacterDB()
+        val characterDatabase: CharacterOfTheDayModel? = repository.getCharacterDB()
+        val selectedDate = getCurrentDayOfTheYear()
+
+        return if (characterDatabase != null && selectedDate == characterDatabase.selectedDate) {
+            characterDatabase.characterModel
+        } else {
+            val result = generateRandomCharacter()
+
+            repository.saveCharacterOfTheDay(
+                CharacterOfTheDayModel(
+                    characterModel = result,
+                    selectedDate = selectedDate
+                )
+            )
+            return result
+        }
+    }
+
+    private suspend fun generateRandomCharacter(): CharacterModel {
+        val randomId = (1..826).random()
         return repository.getSingleCharacter(randomId.toString())
     }
 
