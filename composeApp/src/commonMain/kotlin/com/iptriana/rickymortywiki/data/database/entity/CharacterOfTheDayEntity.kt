c199b84b29be3d38dde92d2c234d3b27f4bcd8ab
Ibrahin <iptrianaa@gmail.com>
2024-11-15 04:51:56 -0600

    updating CharacterOfTheDayEntity

--- composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/data/database/entity/CharacterOfTheDayEntity.kt
+++ composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/data/database/entity/CharacterOfTheDayEntity.kt
@@ -2,6 +2,8 @@ package com.iptriana.rickymortywiki.data.database.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.iptriana.rickymortywiki.domain.model.CharacterModel
+import com.iptriana.rickymortywiki.domain.model.CharacterOfTheDayModel
 
 @Entity(tableName = "character_of_the_day")
 data class CharacterOfTheDayEntity(
@@ -10,4 +12,15 @@ data class CharacterOfTheDayEntity(
     val name: String,
     val image: String,
     val selectedDate: String
-)
+) {
+    fun toDomain(): CharacterOfTheDayModel? {
+        return CharacterOfTheDayModel(
+            characterModel = CharacterModel(
+                id = id,
+                isAlive = isAlive,
+                name = name,
+                image = image
+            ),
+            selectedDate = selectedDate)
+    }
+}
