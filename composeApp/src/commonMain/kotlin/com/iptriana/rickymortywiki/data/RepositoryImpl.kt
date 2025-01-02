2452f1adbbf75d19562df6e8124096b9c6d2e93b
Ibrahin <iptrianaa@gmail.com>
2025-01-01 18:24:46 -0600

    updated RepositoryImpl

--- composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/data/RepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/data/RepositoryImpl.kt
@@ -4,7 +4,6 @@ import androidx.paging.PagingConfig
 import app.cash.paging.Pager
 import app.cash.paging.PagingData
 import com.iptriana.rickymortywiki.data.database.RickyMortyDatabase
-import com.iptriana.rickymortywiki.data.database.entity.CharacterOfTheDayEntity
 import com.iptriana.rickymortywiki.data.remote.ApiService
 import com.iptriana.rickymortywiki.data.remote.paging.CharacterPagingSource
 import com.iptriana.rickymortywiki.domain.Repository
