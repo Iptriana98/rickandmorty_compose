e352102cf0583b85c330b3c98f26b963779a98e1
Ibrahin <iptrianaa@gmail.com>
2025-01-03 16:55:57 -0600

    updated episode screen

--- composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/ui/home/tabs/episodes/EpisodesViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/ui/home/tabs/episodes/EpisodesViewModel.kt
@@ -1,7 +1,18 @@
 package com.iptriana.rickymortywiki.ui.home.tabs.episodes
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import com.iptriana.rickymortywiki.domain.Repository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
-class EpisodesViewModel: ViewModel() {
+class EpisodesViewModel(private val repository: Repository): ViewModel() {
+    private val _state: MutableStateFlow<EpisodesState> = MutableStateFlow(EpisodesState())
+    val state: StateFlow<EpisodesState> = _state
 
+    init {
+        _state.update { state -> state.copy(episodes = repository.getAllEpisodes().cachedIn(viewModelScope)) }
+    }
 }
