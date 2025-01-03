3ab4d56460cbc7fbd4268f7c25595fac7a7ed281
Ibrahin <iptrianaa@gmail.com>
2025-01-03 16:55:38 -0600

    updated repository

--- composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/data/RepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/data/RepositoryImpl.kt
@@ -6,13 +6,18 @@ import app.cash.paging.PagingData
 import com.iptriana.rickymortywiki.data.database.RickyMortyDatabase
 import com.iptriana.rickymortywiki.data.remote.ApiService
 import com.iptriana.rickymortywiki.data.remote.paging.CharacterPagingSource
+import com.iptriana.rickymortywiki.data.remote.paging.EpisodesPagingSource
 import com.iptriana.rickymortywiki.domain.Repository
 import com.iptriana.rickymortywiki.domain.model.CharacterModel
 import com.iptriana.rickymortywiki.domain.model.CharacterOfTheDayModel
+import com.iptriana.rickymortywiki.domain.model.EpisodeModel
 import kotlinx.coroutines.flow.Flow
 
 class RepositoryImpl(
-    private val api: ApiService, private val characterPagingSource: CharacterPagingSource, private val database: RickyMortyDatabase
+    private val api: ApiService,
+    private val characterPagingSource: CharacterPagingSource,
+    private val episodesPagingSource: EpisodesPagingSource,
+    private val database: RickyMortyDatabase
 ) : Repository {
 
     companion object {
@@ -26,6 +31,10 @@ class RepositoryImpl(
     override fun getAllCharacters(): Flow<PagingData<CharacterModel>> =
         Pager(config = PagingConfig(pageSize = MAX_ITEMS, prefetchDistance = PREFETCH_DISTANCE),
             pagingSourceFactory = { characterPagingSource }).flow
+
+    override fun getAllEpisodes(): Flow<androidx.paging.PagingData<EpisodeModel>> =
+        Pager(config = PagingConfig(pageSize = MAX_ITEMS, prefetchDistance = PREFETCH_DISTANCE),
+            pagingSourceFactory = { episodesPagingSource }).flow
 
     override suspend fun getCharacterDB(): CharacterOfTheDayModel? =
         database.getPreferencesDao().getCharacterOfTheDay()?.toDomain()
