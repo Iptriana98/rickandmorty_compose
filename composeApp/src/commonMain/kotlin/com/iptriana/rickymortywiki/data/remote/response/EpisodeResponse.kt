f331921f3796e0c2190f2d76ae4d349a6585b9a4
Ibrahin <iptrianaa@gmail.com>
2025-01-02 20:54:41 -0600

    updated episode response

--- composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/data/remote/response/EpisodeResponse.kt
+++ composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/data/remote/response/EpisodeResponse.kt
@@ -8,23 +8,22 @@ import kotlinx.serialization.Serializable
 data class EpisodeResponse(
     val id: Int,
     val name: String,
-    val airDate: String,
     val episode: String,
     val characters: List<String>,
     val url: String,
-    val created: String
 ) {
-    fun toDomain(): EpisodeModel = EpisodeModel(
-        id = id,
-        name = name,
-        airDate = airDate,
-        episode = episode,
-        characters = characters.map { url -> url.substringAfter("/") },
-        url = url,
-        created = created,
-        season = getSeasonFromEpisodeCode(url),
-        videoUrl = getUrlFromSeason(getSeasonFromEpisodeCode(url))
-    )
+    fun toDomain(): EpisodeModel {
+        val season = getSeasonFromEpisodeCode(episode)
+        return EpisodeModel(
+            id = id,
+            name = name,
+            episode = episode,
+            characters = characters.map { url -> url.substringAfter("/") },
+            url = url,
+            season = season,
+            videoUrl = getUrlFromSeason(season)
+        )
+    }
 
     private fun getUrlFromSeason(season: SeasonEpisode): String = when (season) {
         SeasonEpisode.SEASON_1 -> "https://www.youtube.com/watch?v=8BEzj2kRjO8&ab_channel=RottenTomatoesTV"
@@ -38,13 +37,13 @@ data class EpisodeResponse(
     }
 
     private fun getSeasonFromEpisodeCode(episode: String): SeasonEpisode = when {
-        episode.contains("S01") -> SeasonEpisode.SEASON_1
-        episode.contains("S02") -> SeasonEpisode.SEASON_2
-        episode.contains("S03") -> SeasonEpisode.SEASON_3
-        episode.contains("S04") -> SeasonEpisode.SEASON_4
-        episode.contains("S05") -> SeasonEpisode.SEASON_5
-        episode.contains("S06") -> SeasonEpisode.SEASON_6
-        episode.contains("S07") -> SeasonEpisode.SEASON_7
+        episode.startsWith("S01") -> SeasonEpisode.SEASON_1
+        episode.startsWith("S02") -> SeasonEpisode.SEASON_2
+        episode.startsWith("S03") -> SeasonEpisode.SEASON_3
+        episode.startsWith("S04") -> SeasonEpisode.SEASON_4
+        episode.startsWith("S05") -> SeasonEpisode.SEASON_5
+        episode.startsWith("S06") -> SeasonEpisode.SEASON_6
+        episode.startsWith("S07") -> SeasonEpisode.SEASON_7
         else -> SeasonEpisode.UNKNOWN
     }
 }
