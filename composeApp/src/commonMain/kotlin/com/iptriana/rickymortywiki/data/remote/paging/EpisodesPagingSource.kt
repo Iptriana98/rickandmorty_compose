fa87486ca53a0355cfd727733a8fb3ccdcad34a6
Ibrahin <iptrianaa@gmail.com>
2025-01-03 16:54:16 -0600

    updated episode paging source

--- composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/data/remote/paging/EpisodesPagingSource.kt
+++ composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/data/remote/paging/EpisodesPagingSource.kt
@@ -4,7 +4,6 @@ import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.iptriana.rickymortywiki.data.remote.ApiService
 import com.iptriana.rickymortywiki.domain.model.EpisodeModel
-import io.ktor.utils.io.errors.IOException
 
 class EpisodesPagingSource(private val api: ApiService) : PagingSource<Int, EpisodeModel>() {
     override fun getRefreshKey(state: PagingState<Int, EpisodeModel>): Int? = state.anchorPosition
@@ -12,11 +11,11 @@ class EpisodesPagingSource(private val api: ApiService) : PagingSource<Int, Epis
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, EpisodeModel> = try {
         val page = params.key ?: 1
         val response = api.getAllEpisodes(page)
-        val episodes = response.results.map { it.toDomain() }
+        val episodes = response.results
         val prevKey = if (page > 1) page - 1 else null
-        val nextKey = if (episodes.isNotEmpty()) page + 1 else null
-        LoadResult.Page(data = episodes, prevKey = prevKey, nextKey = nextKey)
-    } catch (e: IOException) {
+        val nextKey = if (response.info.next != null) page + 1 else null
+        LoadResult.Page(data = episodes.map { it.toDomain() }, prevKey = prevKey, nextKey = nextKey)
+    } catch (e: Exception) {
         LoadResult.Error(e)
     }
 }
