3e8ee3ae646d68b01fb59fce3dfe9225bb325f65
Ibrahin <iptrianaa@gmail.com>
2025-01-02 20:54:31 -0600

    updated episode model

--- composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/domain/model/EpisodeModel.kt
+++ composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/domain/model/EpisodeModel.kt
@@ -3,11 +3,9 @@ package com.iptriana.rickymortywiki.domain.model
 data class EpisodeModel(
     val id: Int,
     val name: String,
-    val airDate: String,
     val episode: String,
     val characters: List<String>,
     val url: String,
-    val created: String,
     val videoUrl: String,
     val season: SeasonEpisode
 )
