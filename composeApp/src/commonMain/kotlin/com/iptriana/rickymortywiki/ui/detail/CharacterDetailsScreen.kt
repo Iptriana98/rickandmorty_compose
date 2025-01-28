c06cdfd24643e889e9b5c15f7635c7a80d165d34
Ibrahin <iptrianaa@gmail.com>
2025-01-28 16:24:30 -0600

    Add CharacterDetailsScreen composable.

---
+++ composeApp/src/commonMain/kotlin/com/iptriana/rickymortywiki/ui/detail/CharacterDetailsScreen.kt
@@ -0,0 +1,10 @@
+package com.iptriana.rickymortywiki.ui.detail
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.iptriana.rickymortywiki.domain.model.CharacterModel
+
+@Composable
+fun CharacterDetailsScreen(modifier: Modifier = Modifier, characterModel: CharacterModel) {
+
+}
